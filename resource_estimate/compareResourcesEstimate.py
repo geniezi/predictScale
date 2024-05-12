1466700d743a4181d723c457e47689a2a4d9d735
geniezi <gezhangyang@outlook.com>
2024-05-12 18:01:28 +0800

    resource_estimate

--- resource_estimate/compareResourcesEstimate.py
+++ resource_estimate/compareResourcesEstimate.py
@@ -1,6 +1,8 @@
 import csv
 import importlib
 import math
+import os
+from joblib import dump
 
 import numpy as np
 import pandas as pd
@@ -46,9 +48,12 @@ def new_random_forest(X_train, X_test, y_train, y_test):
     rf_model.fit(X_train, y_train)
 
     # 使用训练好的模型进行预测
-    y_pred = np.round(rf_model.predict(X_test)).astype(int)
+    # y_pred = np.round(rf_model.predict(X_test)).astype(int)
     y_pred = rf_model.predict(X_test)
     rmse_and_mse_mae_compute(y_test, y_pred, "rdf")
+
+    # 使用joblib保存模型
+    dump(rf_model, 'resource_estimate/random_forest_model.joblib')
 
 
 # 决策树
@@ -66,7 +71,7 @@ def new_decision_tree(X_train, X_test, y_train, y_test):
 
 
 # 线性回归
-def new_linear_regression(X_train, X_test, y_train, y_test,scaler_x,scaler_y):
+def new_linear_regression(X_train, X_test, y_train, y_test, scaler_x, scaler_y):
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=False)
 
     # 创建线性回归模型
@@ -84,7 +89,7 @@ def new_linear_regression(X_train, X_test, y_train, y_test,scaler_x,scaler_y):
 
 
 # SVR
-def new_svr(X_train, X_test, y_train, y_test,scaler_x,scaler_y):
+def new_svr(X_train, X_test, y_train, y_test, scaler_x, scaler_y):
     # 将数据集分为训练集和测试集
     # X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42,shuffle=False)
 
@@ -142,7 +147,7 @@ def new_naive_bayes(X_train, X_test, y_train, y_test):
     rmse_and_mse_mae_compute(y_test, y_pred, "naive_bayes")
 
 
-def new_ridge_regression(X_train, X_test, y_train, y_test,scaler_x,scaler_y):
+def new_ridge_regression(X_train, X_test, y_train, y_test, scaler_x, scaler_y):
     # 创建岭回归模型
     ridge = Ridge()
 
@@ -158,6 +163,8 @@ def new_ridge_regression(X_train, X_test, y_train, y_test,scaler_x,scaler_y):
 
 
 if __name__ == "__main__":
+    # 打印当前路径
+    os.system('pwd')
     # 获取当前模块的名称
     current_module = importlib.import_module('__main__')
     # 数据处理
@@ -194,10 +201,10 @@ if __name__ == "__main__":
     y_train_scale = scaler_y.fit_transform(np.array(y_train).reshape(-1, 1))
     y_test_scale = scaler_y.transform(np.array(y_test).reshape(-1, 1))
 
-    new_linear_regression(X_train_scaled, X_test_scale, y_train_scale, y_test_scale,scaler_x,scaler_y)
+    new_linear_regression(X_train_scaled, X_test_scale, y_train_scale, y_test_scale, scaler_x, scaler_y)
     # 岭回归
-    new_ridge_regression(X_train_scaled, X_test_scale, y_train_scale, y_test_scale,scaler_x,scaler_y)
-    new_svr(X_train_scaled, X_test_scale, y_train_scale.ravel(), y_test_scale,scaler_x,scaler_y)
+    new_ridge_regression(X_train_scaled, X_test_scale, y_train_scale, y_test_scale, scaler_x, scaler_y)
+    new_svr(X_train_scaled, X_test_scale, y_train_scale.ravel(), y_test_scale, scaler_x, scaler_y)
 
     # 朴素贝叶斯
     # new_naive_bayes(X_train, X_test, y_train, y_test)
